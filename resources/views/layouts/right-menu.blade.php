75aed8672201f85f32cd057587c053eb8a7834e9
abouhir <abouhir14@gmail.com>
2022-03-25 12:29:23 +0100

    test deployment

--- resources/views/layouts/right-menu.blade.php
+++ resources/views/layouts/right-menu.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -13,6 +14,8 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/alert.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/imageDisplay.js') }}" type="text/javascript"></script>
+
+    <link rel="icon" href="{{asset('images/wlidat-lkhir.jpg')}}" />
     
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
