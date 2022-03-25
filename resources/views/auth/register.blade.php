4976aebe7e1d44884c630e8cd46f860464b7c1d3
abouhir <abouhir14@gmail.com>
2022-03-25 11:10:40 +0100

    test deployment

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,7 +29,7 @@
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                       
 
                         <div class="row mb-3">
                             <label for="name" class="col-md-4 col-form-label text-md-end text-show text-lbl   fs-5">{{ __('الإسم') }}</label>
